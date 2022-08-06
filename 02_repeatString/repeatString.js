32cc39377ba70bf95f2cfe7507db95ba4294c2b4
BelleProjects <bautistadana55@gmail.com>
2022-08-06 02:14:38 -0600

    Add code that passed test 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let count = 0;
+    let output = "";
+    while (count != num) {
+        output += string;
+        count++;
+    }
+    return output;
 };
 
 // Do not edit below this line
