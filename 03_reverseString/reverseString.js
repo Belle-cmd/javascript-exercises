5019426f7a5f4fb16ceaa0591eb8519cd18b8625
BelleProjects <bautistadana55@gmail.com>
2022-08-06 02:52:22 -0600

    Add exercise 3 code that pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = "";
+    let count = 0;
+    const arrayString = input.split("");  // turns the string into an array to enable iteration
+    let index = input.length - 1;  // since array starts at 0, index should be -1 the length of string
+    while (count != input.length) {
+        output += arrayString[index]
+        index--;
+        count++;
+    }
+    return output;
 };
 
 // Do not edit below this line
