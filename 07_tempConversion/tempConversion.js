1a10da9d0a6721e7c4e42d43c5c420b598b56035
BelleProjects <bautistadana55@gmail.com>
2022-08-07 01:47:24 -0600

    Add 2nd function code that pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,11 @@
 const ftoc = function(fahrenheit) {
-  let output = (fahrenheit - 32) * (5/9);
+  const output = (fahrenheit - 32) * (5/9);
   return Math.round(output * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const output = (celsius * (9/5)) + 32;
+  return  Math.round(output * 10) / 10;
 };
 
 // Do not edit below this line
