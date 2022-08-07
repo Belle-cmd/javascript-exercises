6ecce61260c548ea2122598f4faae6ea70bb6f7c
BelleProjects <bautistadana55@gmail.com>
2022-08-07 00:43:16 -0600

    Add exercise 4 code that pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    // iterate through all array arguments
+    let argLength = arguments.length - 1;
+    while (argLength != 0) {
+        // iterate through array and delete last element if it matches target elements
+        let index = 0;
+        while (index < array.length) {
+            // enable duplicated items to be deleted
+            if (array[index] === arguments[argLength]) {
+                array.splice(index, 1); 
+            } else {
+                index++;
+            }
+        }
+        argLength--;
+    }
+    return array;
 };
 
 // Do not edit below this line
