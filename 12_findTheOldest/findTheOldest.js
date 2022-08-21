d45d49c5a78fff7403cac885d14177979bf65466
BelleProjects <bautistadana55@gmail.com>
2022-08-21 02:27:24 -0600

    Add code passing all tests for findTheOldest()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayObj) {
+    const oldest = arrayObj.reduce((prevPerson, curPerson) => {
+        if (!("yearOfDeath" in prevPerson)) prevPerson["yearOfDeath"] = new Date().getFullYear();
+        const age1 = curPerson["yearOfDeath"] - curPerson["yearOfBirth"];
+        const age2 = prevPerson["yearOfDeath"] - prevPerson["yearOfBirth"];
+        if (age1 > age2) {
+            return curPerson;
+        }
+        return prevPerson;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
