16d0018fb6d18fc396d820d717db784d3d747f67
BelleProjects <bautistadana55@gmail.com>
2022-08-20 20:13:41 -0600

    Add sum() code, passing all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,12 +2,16 @@ const add = function(addend1, addend2) {
 	return addend1 + addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, subtrahend) {
+	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
 const multiply = function() {
