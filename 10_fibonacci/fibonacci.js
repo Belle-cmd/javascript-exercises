49a6601152d0265088a4b7ecbca203a8b6f64722
BelleProjects <bautistadana55@gmail.com>
2022-08-20 22:30:51 -0600

    Add code passing all tests for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (typeof(n) === "string") n = Number(n);
 
+	if (n===0) {
+        return 0;
+    } else if (n===1) {
+        return 1;
+    }
+  return fibonacci(n-2) + fibonacci(n-1);
 };
 
 // Do not edit below this line
