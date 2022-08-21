3dc2897a3b7f86467b1cd9fa87798cc888f46b15
BelleProjects <bautistadana55@gmail.com>
2022-08-20 22:20:11 -0600

    Add code passing all tests for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // regex say "anything not lowercase letters" + global 
+    const cleanedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    const splitted = string.toLowerCase().split("");
+    let charOnly = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        if (splitted[i] === " "|| splitted[i] === ","||splitted[i] === "!"||splitted[i] === ".") {
+            continue;
+        }
+        charOnly += splitted[i];
+    }
+    return charOnly === cleanedString;
 };
 
 // Do not edit below this line
