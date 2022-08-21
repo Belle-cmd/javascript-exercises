eccf4bca9231b82cc09f1e3aa7592943abe2ef08
BelleProjects <bautistadana55@gmail.com>
2022-08-21 00:52:49 -0600

    Add code passing all tests for getTheTitles()

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let output = [];
+    obj.filter(item => output.push(item["title"]));
+    return output;
 };
 
 // Do not edit below this line
